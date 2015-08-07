707c39f8cfbc745a13b3260c84f9e4b81d5e408d
HAMM3R67 <tcschauer@gmail.com>
2015-08-06 19:48:56 -0600

    added some syntax notes to the service

--- service.js
+++ service.js
@@ -2,12 +2,27 @@ angular.module('reddit');
 app.service('FirebaseService', function($http, $q){
 	
 	this.getAllPosts = function(){
+		
 		return $q(function(resolve, reject){
 			$.ajax({
 				method: 'GET',
 				url: 'https://devmtn.firebaseio.com/posts.json'
 			})
 		})
+		// Below is the syntax for creating your own promise
+		
+		// var myPromise = $q(function(resolve, reject){
+		//	$http.get('https://devmtn.firebaseio.com/posts.json').then(function(response){
+		//		resolve(response.data)
+		// }, reject)	
+		// })
+		// return myPromise
+		
+		// You could do a simple, elegant way without creating your own promise like below:
+		
+		//$http.get('https://devmtn.firebaseio.com/posts.json').then(function(response){
+		//		return response.data
+		//})
 	}
 	
 	
