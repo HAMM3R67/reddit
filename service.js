454ec7cc568152f42bac9ed546dc752977dfc07f
HAMM3R67 <tcschauer@gmail.com>
2015-08-06 19:34:36 -0600

    built get function in service

--- service.js
+++ service.js
@@ -1,4 +1,15 @@
 angular.module('app');
 app.service('FirebaseService', function($http, $q){
 	
+	this.getPosts = function(){
+		return $q(function(resolve, reject){
+			$.ajax({
+				method: 'GET',
+				url: 'https://devmtn.firebaseio.com/posts.json'
+			})
+		})
+	}
+	
+	
+	
 })
